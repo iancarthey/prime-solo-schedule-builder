2a1fab68ad74dce30796e75fc62702cc07b68c09
Ian Carthey <33433194+iancarthey@users.noreply.github.com>
2018-05-22 21:32:43 -0500

    Update database.sql

--- database.sql
+++ database.sql
@@ -9,33 +9,32 @@ CREATE TABLE schedule_group (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR (100) NOT NULL,
 	active BOOLEAN DEFAULT TRUE,
-	date_created TIMESTAMP,
-	user_id INT REFERENCES person
+	user_id INT REFERENCES person ON DELETE CASCADE
 );
 
 CREATE TABLE schedule (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR (100),
 	date TIMESTAMP,
-	schedule_group_id INT REFERENCES schedule_group
+	schedule_group_id INT REFERENCES schedule_group ON DELETE CASCADE
 );
 
 CREATE TABLE schedule_item (
 	id SERIAL PRIMARY KEY,
 	name VARCHAR (100) NOT NULL,
-	type VARCHAR (100) NOT NULL,
 	github VARCHAR (100),
 	description VARCHAR (1000),
-	schedule_id INT REFERENCES schedule
+	order_id INT,
+	schedule_id INT REFERENCES schedule ON DELETE CASCADE
 );
 
-INSERT INTO schedule_group (name, date_created, user_id) VALUES ('hadar', '4/30/2018', 1);
+INSERT INTO schedule_group (name, user_id) VALUES ('hadar', 1);
 
 INSERT INTO schedule (name, date, schedule_group_id) VALUES ('React-Redux', '5/15/2018', 2);
 
-INSERT INTO schedule_item(name, type, github, description, schedule_id) 
-VALUES ('Redux Lecture', 'lecture', 'N/A', 'fun stuff', 1), 
-('React Lecture', 'lecture','N/A', 'more fun', 1), ('Lunch @ 12', 'misc', 'N/A', 'yummy stuff', 1), 
-('Guest Speaker', 'event', 'N/A', 'interesting stuff', 1), 
-('Redux activity', 'activity', 'N/A', 'group stuff', 1), 
-('Weekend Challenge', 'HW',  'N/A', 'Hard stuff', 1);
+INSERT INTO schedule_item(name, github, description, order_id, schedule_id) 
+VALUES ('Redux Lecture', 'N/A', 'fun stuff', 1, 1), 
+('React Lecture','N/A', 'more fun', 2, 1), ('Lunch @ 12', 'N/A', 'yummy stuff', 3, 1), 
+('Guest Speaker', 'N/A', 'interesting stuff', 4, 1), 
+('Redux activity', 'N/A', 'group stuff', 5, 1), 
+('Weekend Challenge', 'N/A', 'Hard stuff', 6, 1);
